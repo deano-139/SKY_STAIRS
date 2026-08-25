40b10cb67c79aad98fd887bafb93f8ce205e0bc6
tasqlab <200307409+tasqlab@users.noreply.github.com>
2026-08-25 17:33:46 +1000

    fix path

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: '/SKY_STAIRS/', // Moved to the root level
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
-    base: "/SKY_STAIRS/",
     port: 3000,
     strictPort: true,
     hmr: {
